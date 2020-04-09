c0c389ef30fa1bb7ec3e9a475a7df74ff1fa31a0
niickyg <nickyg1028@gmail.com>
2020-04-08 18:23:58 -0700

    changed colors array to be random and created random generator that populates the array

--- rgbGame.js
+++ rgbGame.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDis = document.querySelector("#message")
+
+
 colorDisplay.textContent = pickedColor;
 
 for (i = 0; i<squares.length; i++){
@@ -44,4 +38,26 @@ function colorChanger(){
 function pickColor(){
    var random = Math.floor(Math.random() * colors.length)
    return colors[random];
+}
+
+function generateRandomColors(num){
+    //make array
+    var arr = [];
+    //repeat "num" times
+    for (i =0; i<num; i++){
+        //get color and put it into array;
+       arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+   //red green and blue all need 0 to 255
+   var r = Math.floor(Math.random() * 256);
+
+   var g = Math.floor(Math.random() * 256);
+
+   var b = Math.floor(Math.random() * 256);
+
+   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
